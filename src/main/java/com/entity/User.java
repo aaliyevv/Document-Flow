a7651f7e4751516d6b45107b0a7842600255ffbd
Ali <aaliyev14291@ada.edu.az>
2026-05-01 11:25:03 +0400

    User and enum

---
+++ src/main/java/com/entity/User.java
@@ -0,0 +1,26 @@
+package com.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+}
---
+++ src/main/java/com/entity/enums/DocumentStatus.java
@@ -0,0 +1,9 @@
+package com.entity;
+
+public enum DocumentStatus {
+    SUBMITTED,
+    PENDING_APPROVAL,
+    APPROVED,
+    REJECTED,
+    ARCHIVED
+}
