b94aef038f22ed0172505b710a9a2cd2113488bc
Ali <aaliyev14291@ada.edu.az>
2026-05-08 10:12:34 +0400

    CustomUserDetailsService

--- src/main/java/com/security/CustomUserDetailsService.java
+++ src/main/java/com/security/CustomUserDetailsService.java
@@ -1,4 +1,40 @@
 package com.security;
 
-public class CustomUserDetailsService {
+
+import com.entity.User;
+import com.repo.UserRepository;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+
+// bridge between security and user
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        return mapToUserDetails(user);
+    }
+
+    private UserDetails mapToUserDetails(User user){
+
+        UserBuilder builder = org.springframework.security.core.userdetails.User // not user entity
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRoles().toArray(new String[0]));
+
+        return builder.build();
+
+    }
 }
